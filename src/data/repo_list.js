af2e14597687eac650685374afeae53b5ecbcbc0
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-18 12:54:31 +0100

    yea

--- src/data/repo_list.js
+++ src/data/repo_list.js
@@ -1,50 +1,41 @@
-const repo_list = {
-  'v_to_md5': {
+const repoList = {
+  v_to_md5: {
     repo: 'https://github.com/V-core9/v_to_md5',
     npm: 'https://www.npmjs.com/package/v_to_md5',
   },
-
-  'v_to_sha256': {
+  v_to_sha256: {
     repo: 'https://github.com/V-core9/v_to_sha256',
     npm: 'https://www.npmjs.com/package/v_to_sha256',
   },
-
-  'v_file_system': {
+  v_file_system: {
     repo: 'https://github.com/V-core9/v_file_system',
-    npm: 'https://www.npmjs.com/package/v_file_system',
+    npm: 'https://www.npmjs.com/package/v_file_system'
   },
-
-  'v_scrolls': {
+  v_scrolls: {
     repo: 'https://github.com/V-core9/v_scrolls',
-    npm: 'https://www.npmjs.com/package/v_scrolls',
+    npm: 'https://www.npmjs.com/package/v_scrolls'
   },
-
-  'v_database': {
+  v_database: {
     repo: 'https://github.com/V-core9/v_database',
-    npm: 'https://www.npmjs.com/package/v_database',
+    npm: 'https://www.npmjs.com/package/v_database'
   },
-
-  'v_execute': {
+  v_execute: {
     repo: 'https://github.com/V-tech-tools/v_execute',
-    npm: 'https://www.npmjs.com/package/v_execute',
+    npm: 'https://www.npmjs.com/package/v_execute'
   },
-
   'v-core9_cli': {
     repo: 'https://github.com/V-core9/v-core9_cli',
-    npm: '#',
+    npm: '#'
   },
-
-  'v_database_cli': {
+  v_database_cli: {
     repo: 'https://github.com/V-core9/v_database_cli',
-    npm: 'https://www.npmjs.com/package/v_database_cli',
+    npm: 'https://www.npmjs.com/package/v_database_cli'
   },
 
-  'v_is_empty_value': {
+  v_is_empty_value: {
     repo: 'https://github.com/V-core9/v_is_empty_value',
-    npm: 'https://www.npmjs.com/package/v_is_empty_value',
-  },
-
+    npm: 'https://www.npmjs.com/package/v_is_empty_value'
+  }
 };
 
-
-module.exports = repo_list;
+module.exports = repoList;
